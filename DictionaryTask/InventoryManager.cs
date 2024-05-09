c7f9b28d571545a300257046958c758eef67b8dc
LorincAkos <lorinc.akos13@gmail.com>
2024-05-09 10:45:33 +0200

    Data added

--- DictionaryTask/InventoryManager.cs
+++ DictionaryTask/InventoryManager.cs
@@ -10,11 +10,26 @@ namespace DictionaryTask
         {
             Inventory = new()
             {
-                {5, new Product("name5", HardwareType.GPU, "desc5", 900, 1400)},
                 {1, new Product("name1", HardwareType.GPU, "desc1", 500, 1000)},
                 {2, new Product("name2", HardwareType.CPU, "desc2", 600, 1100)},
                 {3, new Product("name3", HardwareType.RAM, "desc3", 700, 1200)},
                 {4, new Product("name4", HardwareType.RAM, "desc4", 800, 1300)},
+                {5, new Product("name5", HardwareType.GPU, "desc5", 900, 1400)},
+                {6, new Product("name6", HardwareType.GPU, "desc6", 500, 1000)},
+                {7, new Product("name7", HardwareType.CPU, "desc7", 600, 1100)},
+                {8, new Product("name8", HardwareType.MOTHERBOARD, "desc8", 700, 1200)},
+                {9, new Product("name9", HardwareType.MOTHERBOARD, "desc9", 800, 1300)},
+                {10, new Product("name10", HardwareType.STORAGE, "desc10", 500, 1000)},
+                {11, new Product("name11", HardwareType.STORAGE, "desc11", 600, 1100)},
+                {12, new Product("name12", HardwareType.STORAGE, "desc12", 700, 1200)},
+                {13, new Product("name13", HardwareType.MOTHERBOARD, "desc13", 800, 1300)},
+                {14, new Product("name14", HardwareType.CPUCOOLER, "desc14", 500, 1000)},
+                {15, new Product("name15", HardwareType.CPUCOOLER, "desc15", 600, 1100)},
+                {16, new Product("name16", HardwareType.PSU, "desc16", 700, 1200)},
+                {17, new Product("name17", HardwareType.PSU, "desc17", 800, 1300)},
+                {18, new Product("name18", HardwareType.GPU, "desc18", 500, 1000)},
+                {19, new Product("name19", HardwareType.CPU, "desc19", 600, 1100)},
+                {20, new Product("name20", HardwareType.RAM, "desc20", 700, 1200)},
             };
         }
 
@@ -119,7 +134,7 @@ namespace DictionaryTask
                 return;
             }
 
-            Inventory[index - 1].Price = price;
+            Inventory[index ].Price = price;
         }
 
         public void UpdateQuantity()
@@ -140,7 +155,7 @@ namespace DictionaryTask
                 return;
             }
 
-            Inventory[index - 1].Quantity = quantity;
+            Inventory[index].Quantity = quantity;
         }
     }
 }
